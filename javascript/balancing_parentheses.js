7c02d8cd60b5576c4f8d9217e7de664ae17bc96a
Brian Diaz <bidiaz101@gmail.com>
2021-11-11 09:35:13 -0600

    Completed assignment

--- javascript/balancing_parentheses.js
+++ javascript/balancing_parentheses.js
@@ -1,5 +1,23 @@
 function balancingParentheses(string) {
   // type your code here
+
+  let opening = 0
+  let closing = 0
+
+  for(let i = 0; i < string.length; i++) {
+    if(string[i] === "(") {
+      opening++
+      continue
+    } 
+
+    if(opening > 0){
+      opening--
+    } else {
+      closing++
+    }
+  }
+
+  return opening + closing
 }
 
 if (require.main === module) {
@@ -21,4 +39,7 @@ if (require.main === module) {
 module.exports = balancingParentheses;
 
 // Please add your pseudocode to this file
+
+
+
 // And a written explanation of your solution
